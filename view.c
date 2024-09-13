027175ef7d5c8978826cb2571eb012e88e5faa41
Josefinamrm <63682548+Josefinamrm@users.noreply.github.com>
2024-09-13 14:24:25 -0300

    view.c

--- view.c
+++ view.c
@@ -15,13 +15,30 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 
+#define SHM_NAME_SIZE  200
 
-int main(int argc, char * argv[]){ // en el argv[1] tengo el nombre de la shm
+int main(int argc, char * argv[]){
 
     int shm_fd;
     char * addr;
     struct stat copy;
 
+    char shm_name[SHM_NAME_SIZE]={0};
+    // si está en la línea de comandos, el primer argumento 
+    if(argc > 1){
+        strncpy(shm_name, argv[1], sizeof(shm_name)-1);
+        shm_name[strlen(shm_name) + 1] = '\0';
+    }
+    else{
+        // sino, por stdin (pipe)
+        size_t count =read(STDIN_FILENO, shm_name, SHM_NAME_SIZE);
+        shm_name[count+1] = '\0';
+    }
+
+    semaphore * sem = malloc(sizeof(semaphore));
+    sem->mutex = sem_open("mutex", O_CREAT);
+    sem->can_read = sem_open("can_read", O_CREAT);
+    
     shm_fd = shm_open(argv[1], O_RDONLY, 0);
     if(shm_fd == -1){
         perror("shm_open");
@@ -41,13 +58,16 @@ int main(int argc, char * argv[]){ // en el argv[1] tengo el nombre de la shm
 
     char * ptr = addr; // en addr tengo la direcc de memoria de la shm
 
-    for(int i = 0; i < copy.st_size; i++){
+    while(sem_wait(sem->can_read) > -1 ){
+        sem_wait(sem->mutex);
         write(STDOUT_FILENO, ptr, strlen(ptr)); // imprimo el contenido de la shm
-        ptr += (strlen(ptr)+ 1); // avanzo
+        sem_post(sem->mutex);
     }
 
     munmap(addr, copy.st_size);
-    shm_unlink(argv[1]);
+    sem_destroy(sem->mutex);
+    sem_destroy(sem->mutex);
+    shm_unlink(shm_name);
     close(shm_fd);
 
 
